21bc0cd3b32077e2aacc613a5c9e85a951f23fc5
iamscratches <iamscratches@gmail.com>
2020-12-23 12:04:17 +0530

    trying

---
+++ 1.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import os
+import time
+import playsound
+import speech_recognition as sr
+from gtts import gTTS
+
+def speak(text):
+    tts = gTTS(text=text, lang = "en")
+    filename = "voice1.mp3"
+    tts.save(filename)
+    playsound.playsound(filename)
+speak("hello suvnkr")
+
